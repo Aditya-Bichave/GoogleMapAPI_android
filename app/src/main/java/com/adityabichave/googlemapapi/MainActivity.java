2de5a8307e6cb5529dd6684e7773dfa11751079f
Aditya Bichave <aditya.bichave@gmail.com>
2019-04-21 00:30:01 +0530

    Updated

--- app/src/main/java/com/adityabichave/googlemapapi/MainActivity.java
+++ app/src/main/java/com/adityabichave/googlemapapi/MainActivity.java
@@ -1,13 +1,58 @@
 package com.adityabichave.googlemapapi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.googleMap);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        googleMap.addMarker(markerOptions);
+
+        if (googleMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        else
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
     }
 }
